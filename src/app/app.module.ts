a73102d086790f65ac2d34afd7f26dfea0cd11af
umamahesh <umamahesh619ra@gmail.com>
2019-08-18 10:31:40 +0530

    removed old components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { PhotoFramesComponent } from './photo-frames/photo-frames.component';
-import { ResumeComponent } from './resume/resume.component';
 import { ImageService } from 'src/services/image.service';
-import { ForumComponent } from './forum/forum.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Users } from './services/apirequest.services';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WeatherComponent } from './weather/weather.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    PhotoFramesComponent,
-    ResumeComponent,
-    ForumComponent
+   WeatherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
   ],
--- src/app/weather/weather.component.spec.ts
+++ src/app/weather/weather.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { WeatherComponent } from './weather.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('WeatherComponent', () => {
+  let component: WeatherComponent;
+  let fixture: ComponentFixture<WeatherComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ WeatherComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(WeatherComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
